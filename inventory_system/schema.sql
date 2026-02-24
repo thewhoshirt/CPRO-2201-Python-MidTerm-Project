dde6cfc453a0a9b3dd5efdd2dbf5e93011e05db7
Sydney <s.embury.36@gmail.com>
2026-02-24 14:40:59 -0700

    start of schema design

--- inventory_system/schema.sql
+++ inventory_system/schema.sql
@@ -1,1 +1,40 @@
 -- Table Creation Script
+
+-- Required Tables
+-- • Products
+-- • Electronics (for electronics-specific data)
+-- • Perishables (for perishable-specific data)
+-- • Sales (to store transaction history)
+
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Electronics;
+DROP TABLE IF EXISTS Perishables;
+DROP TABLE IF EXISTS Sales;
+
+CREATE TABLE Products(
+    product_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL, 
+    price INTEGER NOT NULL, 
+    stock_quantity INTEGER 
+)
+
+CREATE TABLE Electronics(
+    product_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL, 
+    price INTEGER NOT NULL, 
+    stock_quantity INTEGER,  
+    warranty_period INTEGER, 
+
+    FOREIGN KEY(product_id, name, price, stock_quantity) REFERENCES Products(product_id)
+)
+-- Perishables Table 
+
+
+
+CREATE TABLE Sales(
+    sales_id INTEGER PRIMARY KEY NOT NULL, 
+    product_id INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+
+    FOREIGN KEY(product_id) REFERENCES Products(product_id)
+)
