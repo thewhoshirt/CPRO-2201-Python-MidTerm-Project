f7b2d39843bbef2d47fc15577e6a6dea36dd5dfb
janelle-and <janelle.andrews@rdpolytech.ca>
2026-03-05 14:17:20 -0700

    sales stuff

--- inventory_system/inventory.py
+++ inventory_system/inventory.py
@@ -60,50 +60,31 @@ class Inventory:
     # Add new products
     # -------------------------
     def add_electronic(self, electronic):
-        try:
-            add_electronic(electronic.name, electronic.price, electronic.stock_quantity, electronic.warranty_period)
+        add_electronic(electronic.name, electronic.price, electronic.stock_quantity, electronic.warranty_period)
 
-            self.list_all_products()
-
-            return "Product added successfully"
-        except Exception:
-            return "Product wasn't able to be added"
+        self.list_all_products()
         
     def add_perishable(self, perishable):
-        try:
-            add_perishables(perishable.name, perishable.price, perishable.stock_quantity, perishable.expiration_date)
+        add_perishables(perishable.name, perishable.price, perishable.stock_quantity, perishable.expiration_date)
 
-            self.list_all_products()
-
-            return "Product added successfully"
-        except Exception:
-            return "Product wasn't able to be added"
+        self.list_all_products()
 
     # -------------------------
     # Delete product
     # -------------------------
     def delete_product(self, product_id):
-        try:
-            delete_product(product_id)
+        delete_product(product_id)
 
-            self.list_all_products()
-
-            return "Deletion successful"
-        except Exception:
-            return "Deletion unsucceful"
+        self.list_all_products()
 
     # -------------------------
     # Update product
     # -------------------------
     def update_price(self, product, new_price):
-        try:
-            product.update_price(new_price)
-            update_price(product.product_id, new_price)
+        product.update_price(new_price)
+        update_price(product.product_id, new_price)
 
-            self.list_all_products()
-            return "Update successful"
-        except Exception:
-            return "Update unsuccessful"
+        self.list_all_products()
 
     def update_stock(self, product, quantity):
         in_stock = product.update_stock(quantity)
@@ -119,7 +100,7 @@ class Inventory:
     # -------------------------
     def sale(self, product, quantity):
         in_stock = self.update_stock(product, quantity)
-        if in_stock != "Out of stock":
+        if in_stock:
             sale = Sales(product.product_id, 1, quantity, product.price * quantity)
             try:
                 add_sale(sale.product_id, sale.quantity, sale.price)
@@ -134,22 +115,17 @@ class Inventory:
     def display_sales(self):
         self.sales.clear()
         all_sales = get_all_sales()
-       
-        if len(all_sales) > 0:
-            for s in all_sales:
-                self.sales.append(Sales(s.sales_id, s.product_id, s.quantity, s.price).get_sales_details())
-            
-            return self.sales
+        for s in all_sales:
+            self.sales.append(Sales(s.sales_id, s.product_id, s.quantity, s.price).get_sales_details())
+        return self.sales
 
-        else:
-            return "No sales yet."
 
 
         
 # # testing
-# i = Inventory()
-# e = Electronics(1,"TV",500,10,3)
-# p = Perishables(1,"Apples",4,10,"02-10-2026")
+i = Inventory()
+e = Electronics(1,"TV",500,10,3)
+p = Perishables(1,"Apples",4,10,"02-10-2026")
 # i.add_perishable(p)
 # i.add_electronic(e)
 # print(i.list_all_products())
@@ -163,8 +139,8 @@ class Inventory:
 # print(e.update_stock(10))
 # print(e.get_product_details())
 
-# print(i.sale(p,10))
-# print(i.display_sales())
-# print(i.list_all_products())
+print(i.sale(p,14))
+print(i.display_sales())
+print(i.list_all_products())
 
 
